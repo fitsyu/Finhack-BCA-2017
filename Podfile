eceb3135a4dc26438f892de629aa7500f35e407d
fitsyu <fitrahsyuhada@rocketmail.com>
2018-10-17 19:54:50 +0700

    add notification feature

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FBCA2017' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Alamofire', '~> 4.4'
+  pod 'SwiftyJSON'
+
+  pod 'BDatePicker'
+  
+  pod 'NVActivityIndicatorView', '3.6.0'
+
+
+  # Pods for FBCA2017
+
+end
---
+++ icons/make_icons.sh
@@ -0,0 +1,30 @@
+
+// iPhone
+sips -z 40 40 icon.png --out 20x2.png
+sips -z 60 60 icon.png --out 20x3.png
+
+sips -z 58 58 icon.png --out 29x2.png
+sips -z 87 87 icon.png --out 29x3.png
+
+sips -z 80 80   icon.png --out 40x2.png
+sips -z 120 120 icon.png --out 40x3.png
+
+sips -z 120 120 icon.png --out 60x2.png
+sips -z 180 180 icon.png --out 60x3.png
+
+//iPad
+sips -z 20 20 icon.png --out ipad-20x1.png
+sips -z 40 40 icon.png --out ipad-20x2.png
+
+sips -z 29 29 icon.png --out ipad-29x1.png
+sips -z 58 58 icon.png --out ipad-29x2.png
+
+sips -z 40 40 icon.png --out ipad-40x1.png
+sips -z 80 80 icon.png --out ipad-40x2.png
+
+sips -z 76 76 icon.png   --out ipad-76x1.png
+sips -z 152 152 icon.png --out ipad-76x2.png
+
+sips -z 167 167 icon.png --out ipad-83.5x2.png
+
+
